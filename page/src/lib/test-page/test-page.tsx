6483bb36865163d27f72bd55a3a243677b3d6670
Artem Darius Weber <mit.3tlasa@gmail.com>
2024-04-06 19:55:45 +0300

    feat: mock ui module for profile tests page

--- page/src/lib/test-page/test-page.tsx
+++ page/src/lib/test-page/test-page.tsx
@@ -10,6 +10,7 @@ export function TestPage(props: TestPageProps) {
 
   return (
     <div className={styles['container']}>
+      <div className='mt-8 space-y-4 container mx-auto select-none px-4'>
       <button onClick={() => navigate(-1)} className='flex items-center justify-center space-x-2 ml-4'>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
           <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
@@ -17,14 +18,30 @@ export function TestPage(props: TestPageProps) {
         <p>Назад</p>
       </button>
 
-      <h4 className="text-[24px]">Тесты в городе Краснодар</h4>
+      <h4 className="text-[64px]">Тесты в городе Краснодар</h4>
 
-      <div className='mt-8 space-y-4'>
-        
-        <div className='px-4'>
-          <img src={ParkKrasnodarImage} className="w-full rounded-xl" alt="" />
-        </div>
+        <RouterNavLink to={'/'}>
+          <div className='px-4 mt-12 relative'>
+            <img src={ParkKrasnodarImage} className="w-full max-h-[40em] rounded-xl" alt="" />
+            <div className="absolute left-[7em] bottom-[7em]">
+              <h4 className="text-[60px] font-bold text-white">Тест "Парк Краснодар"</h4>
+              <p className='text-[30px] text-white'>Доступен с 06.04.24 до 08.04.24</p>
+            </div>
+          </div>
+        </RouterNavLink>
 
+        <RouterNavLink to={'/'}>
+          <div className='px-4 mt-12 relative'>
+            <img src={ParkKrasnodarImage} className="w-full max-h-[40em] rounded-xl" alt="" />
+            <div className="absolute left-[7em] bottom-[7em]">
+              <h4 className="text-[60px] font-bold text-white">Тест "Парк Краснодар"</h4>
+              <p className='text-[30px] text-white'>Доступен с 06.04.24 до 08.04.24</p>
+            </div>
+          </div>
+        </RouterNavLink>
+
+
+  
       </div>
     </div>
   );
