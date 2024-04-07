bb5e36d0c95c3c54acf557b01a6c127f70498f15
Artem Darius Weber <mit.3tlasa@gmail.com>
2024-04-07 09:31:49 +0300

    feat: added qr code button

--- page/src/lib/client-page/client-page.tsx
+++ page/src/lib/client-page/client-page.tsx
@@ -49,6 +49,19 @@ export function ClientPage(props: ClientPageProps) {
         </RouterNavLink>
       </div>
 
+      <div className={'absolute left-0 top-20 h-[100vh] py-6 px-3 pb-16 flex flex-col justify-between z-50'}>
+        <RouterNavLink
+          to={'/qr'}
+          className="flex items-center justify-center p-2 bg-white hover:bg-gray-100 shadow-md rounded-full text-gray-400 border"
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 4.875c0-.621.504-1.125 1.125-1.125h4.5c.621 0 1.125.504 1.125 1.125v4.5c0 .621-.504 1.125-1.125 1.125h-4.5A1.125 1.125 0 0 1 3.75 9.375v-4.5ZM3.75 14.625c0-.621.504-1.125 1.125-1.125h4.5c.621 0 1.125.504 1.125 1.125v4.5c0 .621-.504 1.125-1.125 1.125h-4.5a1.125 1.125 0 0 1-1.125-1.125v-4.5ZM13.5 4.875c0-.621.504-1.125 1.125-1.125h4.5c.621 0 1.125.504 1.125 1.125v4.5c0 .621-.504 1.125-1.125 1.125h-4.5A1.125 1.125 0 0 1 13.5 9.375v-4.5Z" />
+            <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 6.75h.75v.75h-.75v-.75ZM6.75 16.5h.75v.75h-.75v-.75ZM16.5 6.75h.75v.75h-.75v-.75ZM13.5 13.5h.75v.75h-.75v-.75ZM13.5 19.5h.75v.75h-.75v-.75ZM19.5 13.5h.75v.75h-.75v-.75ZM19.5 19.5h.75v.75h-.75v-.75ZM16.5 16.5h.75v.75h-.75v-.75Z" />
+          </svg>
+
+        </RouterNavLink>
+      </div>
+
       <MapControls
         onChangeZoom={(value) => setZoom(prevState => prevState + value)}
         onToggleTraffic={() => setTrafficShown(prevState => !prevState)}
