8ce0d73898dfc8d5b8a62979ece12c950debcadc
Artem Darius Weber <mit.3tlasa@gmail.com>
2024-04-07 06:32:41 +0300

    feat: added sended tmp for dublicat rejection and back link to the home

--- module/src/lib/qrcode-scanner/qrcode-scanner.tsx
+++ module/src/lib/qrcode-scanner/qrcode-scanner.tsx
@@ -1,12 +1,15 @@
 import styles from './qrcode-scanner.module.scss';
 import React, { useState, useEffect } from 'react';
 import { QrReader } from 'react-qr-reader';
+import { useNavigate, NavLink as RouterNavLink } from 'react-router-dom';
 
 /* eslint-disable-next-line */
 export interface QRCodeScannerProps {}
 
 export function QRCodeScanner(props: QRCodeScannerProps) {
+  const navigate = useNavigate();
   const [data, setData] = useState('No result');
+  const [sended, setSended] = useState("No result");
 
   const handleRegisterSubmit = async () => {
     try {
@@ -25,6 +28,7 @@ export function QRCodeScanner(props: QRCodeScannerProps) {
         const data = await response.json();
         console.log(data);
         console.log('Успешно проверен!');
+        navigate('/');
       } else {
         console.error('Ошибка при проверке');
         console.error(response);
@@ -34,23 +38,26 @@ export function QRCodeScanner(props: QRCodeScannerProps) {
     }
   };
 
-  // useEffect(() => {
-  //   if (data !== "No result") {
-  //     handleRegisterSubmit();
-  //   }
-  // }, [data]);
-
 
   return (
-    <>
-      <QrReader
+    <div className="container">
+        <button onClick={() => navigate(-1)} className='flex mt-8 items-center justify-center space-x-2 ml-4'>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+          </svg>
+          <p>Назад</p>
+        </button>
+        <QrReader
         constraints={{ facingMode: 'environment' }}
         onResult={(result: any, error: any) => {
           if (result) {
             setData(result?.text);
             console.info(result?.text);
-            console.info("KJAKFKFAKAS");
-            handleRegisterSubmit();
+            if (result?.text != sended) {
+              handleRegisterSubmit();
+              console.info("KJAKFKFAKAS");
+              setSended(result?.text);
+            }
           }
           if (error) {
             console.info(error);
@@ -58,7 +65,7 @@ export function QRCodeScanner(props: QRCodeScannerProps) {
         }}
       />
       <p>{data}</p>
-    </>
+      </div>
   );
 }
 
