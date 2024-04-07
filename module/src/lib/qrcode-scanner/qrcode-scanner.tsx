9e046e9db6dad0b32f704ac86fa1e6f1ea4df751
Bagi4 <67656353+Bagi4-source@users.noreply.github.com>
2024-04-07 12:36:21 +0300

    fix: qr-scanner

--- module/src/lib/qrcode-scanner/qrcode-scanner.tsx
+++ module/src/lib/qrcode-scanner/qrcode-scanner.tsx
@@ -1,71 +1,63 @@
 import styles from './qrcode-scanner.module.scss';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { QrReader } from 'react-qr-reader';
 import { useNavigate, NavLink as RouterNavLink } from 'react-router-dom';
 
 /* eslint-disable-next-line */
-export interface QRCodeScannerProps {}
+export interface QRCodeScannerProps {
+}
 
 export function QRCodeScanner(props: QRCodeScannerProps) {
   const navigate = useNavigate();
   const [data, setData] = useState('No result');
-  const [sended, setSended] = useState("No result");
+  const [successed, setSuccessed] = useState(false);
 
-  const handleRegisterSubmit = async () => {
-    try {
-      const response = await fetch('https://hack4.k-lab.su/api/business/qr-code/check/', {
+  useEffect(() => {
+    (async () => {
+      await fetch('https://hack4.k-lab.su/api/business/qr-code/check/', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
-          'Authorization': 'Token ' + localStorage.getItem('access_token') || '',
+          'Authorization': 'Token ' + localStorage.getItem('access_token') || ''
         },
-        body: JSON.stringify({ qrCodeData: data }),
-      });
-
-      console.log("data: ", data);
-
-      if (response.ok) {
-        const data = await response.json();
-        console.log(data);
-        console.log('Успешно проверен!');
-        navigate('/');
-      } else {
-        console.error('Ошибка при проверке');
-        console.error(response);
-      }
-    } catch (error) {
-      console.error('Произошла ошибка:', error);
-    }
-  };
+        body: JSON.stringify({ qrCodeData: data })
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          setSuccessed(true);
+          navigate('/');
+        })
+        .catch((error) => {
+          console.error('Произошла ошибка:', error);
+        })
+    })()
+  }, [data]);
 
 
   return (
     <div className="container">
-        <button onClick={() => navigate(-1)} className='flex mt-8 items-center justify-center space-x-2 ml-4'>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
-          </svg>
-          <p>Назад</p>
-        </button>
+      <button onClick={() => navigate(-1)} className="flex mt-8 items-center justify-center space-x-2 ml-4">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"
+             className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+        </svg>
+        <p>Назад</p>
+      </button>
+      {!successed &&
         <QrReader
-        constraints={{ facingMode: 'environment' }}
-        onResult={(result: any, error: any) => {
-          if (result) {
-            setData(result?.text);
-            console.info(result?.text);
-            if (result?.text != sended) {
-              handleRegisterSubmit();
-              console.info("KJAKFKFAKAS");
-              setSended(result?.text);
+          constraints={{ facingMode: 'environment' }}
+          onResult={(result: any, error: any) => {
+            if (result) {
+              setData(result.text);
             }
-          }
-          if (error) {
-            console.info(error);
-          }
-        }}
-      />
+            if (error) {
+              console.info(error);
+            }
+          }}
+        />
+      }
       <p>{data}</p>
-      </div>
+    </div>
   );
 }
 
